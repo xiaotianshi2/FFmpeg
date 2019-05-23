936b795f33b01f836c670cd471c9545de9ade285
joepadmiraal <joep@exmachina.nl>
2019-05-23 11:28:41 +0200

    Added vmaf to local build

--- make-exmg-lls-linux.sh
+++ make-exmg-lls-linux.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # enable-version3 enables LGPL licence version 3
-./configure --enable-gpl --enable-decklink --enable-openssl --enable-version3 --enable-static --disable-sdl2 --disable-debug --disable-ffplay --disable-libxcb --disable-libxcb-shm --disable-indev=sndio --disable-outdev=sndio --disable-outdev=xv --enable-libx264 --enable-libpulse --enable-nonfree --pkg-config-flags=--static --extra-cflags="-Idecklink"
+# build result written to the output directory so we can easily link against it
+./configure --enable-gpl --enable-openssl --enable-version3 --disable-sdl2 --enable-debug --disable-ffplay --disable-libxcb --disable-libxcb-shm --disable-indev=sndio --disable-outdev=sndio --disable-outdev=xv --enable-libx264 --enable-libpulse --enable-nonfree  --enable-libvmaf --pkg-config-flags=--static --prefix="output"
 
 make -j4
-
-cp ffmpeg ../playtrivia-video-tests/ffmpeg-exmg-lls-linux
-cp ffmpeg ~/go/src/bitbucket.org/exmachina/video-encoder/assets/ffmpeg-exmg-lls-linux
+cp ffmpeg_g ../playtrivia-video-tests/ffmpeg-exmg-lls-linux
+cp ffmpeg_g ~/go/src/bitbucket.org/exmachina/video-encoder/assets/ffmpeg-exmg-lls-linux
