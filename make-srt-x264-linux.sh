5216e724f710bbc0d71c68ce1854c3d42d0ff3bf
xiaotian.shi <xiaotianshimail@gmail.com>
2019-12-17 12:07:44 -0800

    updated for cuda enabled build for testing purpose.

--- make-srt-x264-linux.sh
+++ make-srt-x264-linux.sh
@@ -3,6 +3,7 @@
 # enable-version3 enables LGPL licence version 3
 # build result written to the output directory so we can easily link against it
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/lib/pkgconfig
+export PATH=$PATH:/usr/local/cuda/bin
 ./configure \
   --enable-version3 \
   --disable-sdl2 \
@@ -20,7 +21,10 @@ export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/lib/pkgconfig
   --enable-cuvid \
   --enable-nvenc \
   --enable-libnpp \
+  --extra-cflags="-I/usr/local/cuda/include/" \
+  --extra-ldflags="-L/usr/local/cuda/lib64/" \
   --enable-libx264 \
+  --enable-protocol=libsrt\
   --enable-libsrt \
   --enable-nonfree \
   --disable-shared \
@@ -30,6 +34,7 @@ export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/lib/pkgconfig
 
 make -j4
 cp ffmpeg_g ../exmachina-ffmpeg-example-f9117b8783ef/.
+cp ffmpeg ../exmachina-ffmpeg-example-f9117b8783ef/.
 #note srt configure options ./configure --enable-static --disable-shared --enable-debug
 #cp ffmpeg_g ../playtrivia-video-tests/ffmpeg-exmg-lls-linux
 #cp ffmpeg_g ~/go/src/bitbucket.org/exmachina/video-encoder/assets/ffmpeg-exmg-lls-linux
